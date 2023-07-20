6395223c9dc105ed1af77d123ee259fdf2d6ece4
WarrenHawker <hawker.warren@gmail.com>
2023-07-21 00:53:55 +0100

    fixed more wrong imports

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState } from 'react';
-import { ContextProviderProps, User } from '../lib/interfaces';
+import { ContextProviderProps, User } from '../utils/interfaces';
 
 interface UseAuthProps {
   user: User;
--- src/context/Providers.tsx
+++ src/context/Providers.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ContextProviderProps } from '@/lib/interfaces';
+import { ContextProviderProps } from '@/utils/interfaces';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useState } from 'react';

1542ba5949e65b9b4d04348c494774915cb8a59f
WarrenHawker <hawker.warren@gmail.com>
2023-07-24 17:47:19 +0100

    added auto-hiding nav bar on page load

--- src/app/(admin)/components/layout/HeaderAdmin.tsx
+++ src/app/(admin)/components/layout/HeaderAdmin.tsx
@@ -1,11 +1,15 @@
 'use client';
 import Link from 'next/link';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { usePathname } from 'next/navigation';
 
 const HeaderAdmin = () => {
   const [showTopNav, setShowTopNav] = useState<boolean>(false);
   const pathname = usePathname();
+
+  useEffect(() => {
+    setShowTopNav(false);
+  }, [pathname]);
 
   return (
     <header>
